2a0e2078bb31b30d30280e5b7bc56b33ef8bc4df
bilal <bilal@nearpeer.org>
2023-11-20 12:20:18 +0500

    adding failuer case

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -3,7 +3,7 @@
 it("google-test", function() {
   cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",{ timeout: 600000 });
   cy.get(":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input").type("Admin");
-  cy.get(":nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input").type("admin123");
+  cy.get(":nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input").type("admin1234");
   cy.get(".oxd-button").click();
   cy.get(":nth-child(1) > .oxd-main-menu-item > .oxd-text").click();
 });
